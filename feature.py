bc48e6b48e384ff05e8883b8c29c3c7f6ebc8cc9
Ethan6775 <Eyin26_student@ivycollegiate.org>
2025-10-15 07:55:31 +0000

    Updates python script

---
+++ feature.py
@@ -0,0 +1,13 @@
+#This is a comment
+
+'''
+This is a multiple line comment
+using triple single quotes
+'''
+
+"""
+This is another multiple line
+commit, using triple DOUBLE quotes
+"""
+
+print("Hello world!")
