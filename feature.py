dcf75bdfb0cf0da122df178ff90c7dcfea9e19e7
rtseng26student-spec <rtseng26_student@ivycollegiate.org>
2025-10-15 07:55:35 +0000

    Updates python script

---
+++ feature.py
@@ -0,0 +1,15 @@
+
+'''
+
+This is a multiple line comment
+using triple single wuotes
+'''
+
+
+"""
+
+This is another multiple line 
+comment, using triple DOUBLE quotes
+"""
+
+print("Hello, world!")
