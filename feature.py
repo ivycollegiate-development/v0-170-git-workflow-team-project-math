c725659ecd72fcb2a290a82c6ddc47846a1cbcd4
cli26student-droid <cli26_student@ivycollegiate.org>
2025-10-15 07:55:20 +0000

    Updates python script

---
+++ feature.py
@@ -0,0 +1,13 @@
+# This is a comment
+
+'''
+This is a ultiole line comment
+using triple single quotes
+'''
+
+"""
+This is another multriple line
+comment, using triple DOUBLE quotes
+"""
+
+print("Hello, world!")
